e9c13be36b59c8bd516598e4e382b116c05618ec
Michael Cook <git@mrcook.uk>
2009-10-24 09:36:25 +0000

    Made the DRIVES & PATHS variables more descriptive.

--- pg2files.pl
+++ pg2files.pl
@@ -16,19 +16,23 @@ use constant DS => '/';   ## Default Directory Separator
 
 use vars '$line';
 
-## Default BOOK Drive/Path ##
-$booksfolder    = DS . 'BOOK-Files' . DS . '04-TEI' . DS;  ## Default Books Path
-## Get current drive
-$cur_dir        = File::Spec->curdir();
-$cur_dir        = File::Spec->rel2abs unless ( File::Spec->file_name_is_absolute($cur_dir) );
-$cur_drive      = (File::Spec->splitpath($cur_dir))[0];
-## pg2tei Perl script path
-$pg2tei_script  = $cur_drive . DS . 'epbProject' . DS . 'epb-pg2tei' . DS . 'pg2tei.pl';
+##################################
+## Get correct DRIVES and PATHS ##
+##################################
+$current_dir        = File::Spec->curdir();
+$current_dir        = File::Spec->rel2abs unless ( File::Spec->file_name_is_absolute($current_dir) );
+$current_drive      = (File::Spec->splitpath($current_dir))[0];
+
+# PATH to the TEI book files
+$books_folder    = DS . 'BOOK-Files' . DS . '04-TEI' . DS;  ## Default Books Path
+
+# PATH to pg2tei.pl Perl script
+$pg2tei_script  = $current_drive . DS . 'epbProject' . DS . 'epb-pg2tei' . DS . 'pg2tei.pl';
 
 ######################################
 ## Search and execute all TXT files ##
 ######################################
-find (\&eachTEI, $cur_drive . $booksfolder);
+find (\&eachTEI, $current_drive . $books_folder);
 
 sub eachTEI {
   return unless $_=~ /\.txt$/;    ## Skip non-TXT files
@@ -38,18 +42,18 @@ sub eachTEI {
 
   $bookspath      = $File::Find::name;
   $bookspath      =~ s|/(.*?)/$file|/$1|;
-  $tei_path_file  = $booksfolder . $filename . DS . 'tei' . DS . $filename . '.tei';
+  $tei_path_file  = $books_folder . $filename . DS . 'tei' . DS . $filename . '.tei';
 
   ## create default folder structure (/##/tei/) for each book
-  mkdir($booksfolder . $filename . DS,             0777)  || print "\nCan't create $filename folder: $!\n";
-  mkdir($booksfolder . $filename . DS . 'pg-orig', 0777)  || print "\nCan't create PG_ORIG folder: $!\n";
-  mkdir($booksfolder . $filename . DS . 'tei',     0777)  || print "\nCan't create TEI folder: $!\n";
+  mkdir($books_folder . $filename . DS,             0777)  || print "\nCan't create $filename folder: $!\n";
+  mkdir($books_folder . $filename . DS . 'pg-orig', 0777)  || print "\nCan't create PG_ORIG folder: $!\n";
+  mkdir($books_folder . $filename . DS . 'tei',     0777)  || print "\nCan't create TEI folder: $!\n";
 
   ## Make a backup copy of the .txt file in "pg-orig".
-  copy($file, $booksfolder . $filename . DS . 'pg-orig' . DS . $filename . '.txt') or die "Failed to copy file: $!\n";
+  copy($file, $books_folder . $filename . DS . 'pg-orig' . DS . $filename . '.txt') or die "Failed to copy file: $!\n";
 
   ## Now run the pg2tei.pl script using `backticks` to get file into variable.
-  $pg2tei = `perl -w $pg2tei_script $booksfolder $file`;
+  $pg2tei = `perl -w $pg2tei_script $books_folder $file`;
 
 
   #######################################
